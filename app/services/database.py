b3485c3fd22ed9ffa40b33e2450058cd45ed16bc
charlesb32 <itscharleyy@gmail.com>
2024-07-06 10:43:55 -0400

    can now connect to DB from anywhere as long as my PC is running the server.

--- app/services/database.py
+++ app/services/database.py
@@ -1,10 +1,18 @@
 import mysql.connector
 
+# db_config = {
+#     "host": "127.0.0.1",
+#     "user": "root",
+#     "password": "Capsfan26!",
+#     "database": "unidishdb"
+# }
+
 db_config = {
-    "host": "127.0.0.1",
+    "host": "192.168.1.25",  # replace with the server's IP address
     "user": "charlesb32",
     "password": "Capsfan26!",
-    "database": "unidashdb"
+    "database": "unidishdb",
+    "port": 3306
 }
 
 def get_db_connection():
